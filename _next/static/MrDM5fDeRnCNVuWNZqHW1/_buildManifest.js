99229a0b85581a7e9d497935bf0b3be1605129b9
neferpi <neferpi@users.noreply.github.com>
2026-09-13 19:05:17 +0000

    Deploy Fishmouth static export

---
+++ _next/static/MrDM5fDeRnCNVuWNZqHW1/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fishmouth//_next/:path+",
+        "destination": "/fishmouth/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
